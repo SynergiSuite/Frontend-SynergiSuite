87b6993de77221ce0ffb16423d1a5a24d5394039
irteza-hussain <irtiza.hussain@boxxinsurance.com>
2025-07-02 15:10:29 +0500

    initial setup

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,36 @@
+export default function Testing() {
+    return (
+        <>
+
+        <div className="flex flex-row justify-between">
+            <div className="h-full w-[25%]"></div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="border_primary component_styling component_container_square">
+                    <button className=" my-6 mx-6 button_primary_lg">button-lg</button>
+                    <button className=" my-6 mx-6 button_primary_xl">button-xl</button>
+                    <h3 className="my-6 mx-6 component_heading">Component Heading</h3>
+                    <h4 className="my-6 mx-6 normal_heading">Normal Heading</h4>
+                    <p className="my-6 mx-6 under_heading">Under Heading</p>
+                </div>
+        
+                <div className="border_primary component_styling component_container_square">
+                    <button className=" my-6 mx-6 button_primary_lg">button-lg</button>
+                    <button className=" my-6 mx-6 button_primary_xl">button-xl</button>
+                    <h3 className="my-6 mx-6 component_heading">Component Heading</h3>
+                    <h4 className="my-6 mx-6 normal_heading">Normal Heading</h4>
+                    <p className="my-6 mx-6 under_heading">Under Heading</p>
+                </div>
+
+                <div className="border_primary component_styling component_container_square">
+                    <button className=" my-6 mx-6 button_primary_lg">button-lg</button>
+                    <button className=" my-6 mx-6 button_primary_xl">button-xl</button>
+                    <h3 className="my-6 mx-6 component_heading">Component Heading</h3>
+                    <h4 className="my-6 mx-6 normal_heading">Normal Heading</h4>
+                    <p className="my-6 mx-6 under_heading">Under Heading</p>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
